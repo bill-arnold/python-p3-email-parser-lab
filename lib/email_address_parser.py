00d335fc1ba7090a73d0507c2fb41620a9d9cf46
bill <bill.arnold@student.moringaschool.com>
2023-12-06 18:51:02 +0300

    done

--- lib/email_address_parser.py
+++ lib/email_address_parser.py
@@ -1,1 +1,15 @@
 # your code goes here!
+import re
+
+class EmailAddressParser:
+    def __init__(self, email_addresses):
+        self.email_addresses = email_addresses
+
+    def parse(self):
+        # Use regex to find all email addresses
+        emails = re.findall(r'\S+@\S+', self.email_addresses)
+
+        # Remove duplicates and sort alphabetically
+        unique_emails = sorted(set(emails))
+
+        return unique_emails
